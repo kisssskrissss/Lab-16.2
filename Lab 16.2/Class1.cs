ce79b9a1ccc073157708695e3345c403b75c4384
krist <krist@LAPTOP-HIQLHLL6>
2022-11-27 21:09:52 +0500

    Добавьте файлы проекта.

---
+++ Lab 16.2/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_16._2
+{
+    class Product
+    {
+        public int Num { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ Lab 16.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace Lab_16._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = string.Empty;
+            using (StreamReader sr = new StreamReader("../../../Products.json")) 
+            {
+                jsonString = sr.ReadToEnd();
+            }
+            Product[] products = JsonSerializer.Deserialize<Product[]>(jsonString);
+
+            Product max = products[0];
+            foreach (Product e in products)
+            {
+                if (e.Price>max.Price)
+                {
+                    max = e;
+                }
+                Console.WriteLine($"{max.Num},{max.Name},{max.Price}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
